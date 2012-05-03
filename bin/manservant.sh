3d5d6094678a5d7ad62f7dbfab792ed60897fc09
spartango@gmail.com <spartango@gmail.com>
2012-05-03 16:04:11 -0400

    a simple shell script to call open on OSX

---
+++ bin/manservant.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Command line invokation for manservant (https://github.com/jimeh/manservant)
+# @ Anand Gupta 2012
+
+# Manservant is a pretty way to view man pages in the browser, rendering them
+# in comfortable HTML. 
+
+# This script allows you to bring up the manservant version of any man page 
+# from the command line, such that one could type 'man tmux', and go straight
+# to the browser
+
+# This will only support OSX for the moment, until I find the right "open browser"
+# cli call for other OSes
+
+MANSERVANT_HOST="http://man.dev"
+
+open "$MANSERVANT_HOST/$1"

f98db5ebc6ea48c221570094b9140407afda616e
spartango@gmail.com <spartango@gmail.com>
2012-05-03 23:06:09 -0400

    intermediate commit, with parsing and object construction for the man page

--- bin/manservant_page.rb
+++ bin/manservant_page.rb
@@ -12,15 +12,58 @@
 require 'lib/manservant/man_page'
 require 'launchy'
 
-# Check if there's a cached/open version of the page already
+# Helpers borrowed from the server
+def parse_page_name(dirty_name)
+  name = sanitize_name(dirty_name)
+  redirect to("/#{name}") if name != dirty_name
+  if name =~ /^(.+)\.(\d+?)$/
+    [$1, $2]
+  else
+    [name, nil]
+  end
+end
 
-# Build a standalone manservant page
-    # Create a ManPage object
+def sanitize_name(name)
+  name.downcase.gsub(/[^a-z0-9\-_\.]/i, '')
+end
+
+def find_page(name, section = nil)
+  ManPage.new(name, section,
+    :man2html_path => man2html_path,
+    :man2html_args => { :cgiurl => '\'/${title}.${section}\'' })
+end
+
+def man2html_path
+  File.expand_path('./libexec/man2html', __FILE__)
+end
+
+# Get the target page
+if ARGV.length > 0
+  @desired_prog = ARGV[1]
+  @name, @section = parse_page_name(params[:page])
+
+  # Build a standalone manservant page
+  @page = find_page(@name, @section)
+  begin
     # Render it to HTML
+    @html = @page.to_html # render and populate sections
+
     # Fit that HTML into the template
+
     # Make sure assets are in place
-# Write it out to a /tmp/[file] 
 
-# Launchy open the file
+    # Write it out to a /tmp/[file] 
 
-# Cleanup rendered pages -- we'll need to let the browser work first
+    # Launchy open the file
+
+    # Cleanup rendered pages -- we'll need to let the browser work first
+
+  rescue Manservant::ManPage::NotFound => e
+    print 'No manual entry for ', @name
+    exit 1
+  end
+else
+  # No target page, should just exit
+  print 'What manual page do you want?'
+  exit 1
+end
